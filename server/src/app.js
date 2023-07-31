75cea0cd75b4782c14f7154df8652972ac6cf4bc
Shivam <bishtshivam101@gmail.com>
2023-07-31 16:00:57 +0530

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(cors({
     origin: ['http://localhost:3000',
         'http://localhost:3001',
-        'https://leveranceoxygen.com'],
+        'https://saiyedfarhan.com',
+        'https://admin.saiyedfarhan.com'],
 }))
 
 app.use(express.json())
